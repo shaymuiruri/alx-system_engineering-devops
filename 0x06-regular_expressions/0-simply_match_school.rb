576fb957623db2d9f6a3d599b5a8a549cb0a2e78
Mitchelle Muiruri <muiruri003michelle@gmail.com>
2023-08-01 12:01:58 -0700

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,23 +1,4 @@
 #!/usr/bin/env ruby
 
-def match_school(string)
-  pattern = /School/
-  if string.match?(pattern)
-    puts "The string '#{string}' matches the pattern 'School'."
-  else
-    puts "The string '#{string}' does not match the pattern 'School'."
-  end
-end
-
-# Check if there is exactly one argument provided
-if ARGV.length != 1
-  puts "Usage: ruby regex_match.rb <string>"
-  exit(1)
-end
-
-# Get the first argument from the command line
-input_string = ARGV[0]
-
-# Call the regular expression method
-match_school(input_string)
+puts ARGV[0].scan(/[S]chool/).join
 
