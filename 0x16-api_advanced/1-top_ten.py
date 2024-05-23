9e3b4a57c877b297ca2d468dd40fbad757d60a7c
Mitchelle Muiruri <121823073+shaymuiruri@users.noreply.github.com>
2024-05-23 13:42:58 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-"""
-Module that contains a function
-which queries the Reddit API
-"""
-
-import requests
+"""Module for task 1"""
 
 
 def top_ten(subreddit):
-    """
-    prints the titles of the first 10
-    hot posts listed for a given subreddit
-    """
-    if subreddit is None or type(subreddit) is not str:
-        return 0
-    url = requests.get('http://www.reddit.com/r/{}/hot.json'.format(subreddit),
-                       headers={'User-Agent': 'Python/requests:APIproject:v1.0.0\
-                       (by /u/abukiplimo)'}, params={'limit': 10}).json()
-    posts = url.get('data', {}).get('children', None)
-    if posts is None or (len(posts) > 0 and posts[0].get('kind') != 't3'):
-        print(None)
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        print('None')
     else:
-        for post in posts:
-            print(post.get('data', {}).get('title', None))
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
